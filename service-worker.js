85e4c70dc01d416575888ed1daaa7c1b25c8c432
ガンマ技術部 <yu1000.scratch@gmail.com>
2025-05-25 12:17:56 +0900

    Update and rename service-worker.js to sw.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
-
-self.addEventListener("push", function(event) {
-    const data = event.data.json();
-    self.registration.showNotification(data.notification.title, {
-        body: data.notification.body,
-        icon: "/icon.png"
-    });
-});
